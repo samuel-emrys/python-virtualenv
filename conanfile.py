87eb4a8d83feb2122f9646aa9b6ea4c2414d8308
Samuel Dowling <samuel.dowling@protonmail.com>
2023-01-15 01:30:48 +1030

    feat(compatibility): Add conan 2 compatibility

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,17 @@
-from conans import ConanFile, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 import os
+import sys
 import json
 import textwrap
 
+def _args_to_string(args):
+    if not args:
+        return ""
+    if sys.platform == 'win32':
+        return subprocess.list2cmdline(args)
+    else:
+        return " ".join("'" + arg.replace("'", r"'\''") + "'" for arg in args)
 
 class PythonVirtualEnvironment(ConanFile):
     name = "python-virtualenv"
@@ -18,7 +26,7 @@ class PythonVirtualEnvironment(ConanFile):
     options = {"requirements": "ANY"}
     default_options = {"requirements": "[]"}
 
-    python_requires = "pyvenv/0.1.0"
+    python_requires = "pyvenv/0.1.0@mtolympus/stable"
     # python venvs are not relocatable, so we will not have binaries for this on artifactory. Just build it on first use
     build_policy = "missing"
     _venv = None
@@ -45,7 +53,7 @@ class PythonVirtualEnvironment(ConanFile):
         requirements = json.loads(str(self.options.get_safe("requirements", "[]")))
         if requirements:
             self.run(
-                tools.args_to_string(
+                _args_to_string(
                     [
                         venv.pip,
                         "install",
@@ -54,11 +62,12 @@ class PythonVirtualEnvironment(ConanFile):
                 )
             )
 
+        bindir = "Scripts" if self.settings.os_build == "Windows" else "bin"
         for requirement in requirements:
             package = requirement.split("==")[0]
             # Ensure that there's an entry point for everything we've just installed.
             venv.setup_entry_points(
-                str(package), os.path.join(self.package_folder, "bin")
+                str(package), os.path.join(self.package_folder, bindir)
             )
 
     def package_info(self):
