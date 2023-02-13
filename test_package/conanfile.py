73cc829633de78437b42391039894daccf9a05f5
Samuel Dowling <samuel.dowling@protonmail.com>
2023-02-13 23:38:04 +1030

    4: Add print statement to echo the path in the build context

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -7,7 +7,7 @@ from conan.tools.build import cross_building
 
 class PythonVirtualenvTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "VirtualBuildEnv"
+    generators = "VirtualBuildEnv", "VirtualRunEnv"
     apply_env = False
     test_type = "explicit"
 
@@ -22,11 +22,18 @@ class PythonVirtualenvTestConan(ConanFile):
 
     def test(self):
         if not cross_building(self):
+            path = "$PATH"
+            if self.settings.os == "Windows":
+                path = "%PATH%"
+            self.output.info(f"Executing `echo {path}` with env=\"conanrun\"")
+            self.run(f"echo {path}", env="conanrun")
+            self.output.info(f"Executing `echo {path}` with env=\"conanbuild\"")
+            self.run(f"echo {path}", env="conanbuild")
             # This should exist on the path in the build environment
             requirements = self.conf.get("user.env.pythonenv:requirements")
             env_dir = self.conf.get("user.env.pythonenv:dir")
-            self.output.info(f"{env_dir=}")
-            self.output.info(f"{requirements=}")
+            self.output.info(f"user.env.pythonenv:dir={env_dir}")
+            self.output.info(f"user.env.pythonenv:requirements={requirements}")
             self.output.info("Executing `python --version`")
             self.run("python --version", env="conanbuild")
             self.output.info("Executing `sphinx-build --help`")
